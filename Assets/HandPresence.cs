46fc8b270451fe1f98605a48e4f3d9475471a8c4
ModeAxe <modeaxe@gmail.com>
2020-06-14 21:04:18 -0400

    Set up hand animation based on controller input

--- Assets/HandPresence.cs
+++ Assets/HandPresence.cs
@@ -6,22 +6,75 @@ public class HandPresence : MonoBehaviour
 {
     public List<GameObject> controllers;
     public InputDeviceCharacteristics controllerCharacteristics;
+    public GameObject handModelPrefab;
     public bool showController = false;
 
     private InputDevice targetDevice;
-    public GameObject handModelPrefab;
     private GameObject spawnedHandModel;
     private GameObject spawnedController;
+    private Animator handAnimator;
 
     
     // Start is called before the first frame update
     void Start()
     {
+        tryInitialize();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue);
+        if (!targetDevice.isValid)
+        {
+            tryInitialize();
+        }
+        else
+        {
+
+            if (showController)
+            {
+                spawnedHandModel.SetActive(false);
+                spawnedController.SetActive(true);
+            }
+            else
+            {
+                spawnedController.SetActive(false);
+                spawnedHandModel.SetActive(true);
+                UpdateHandAnimation();
+            }
+        }
+    }
+
+    void UpdateHandAnimation()
+    {
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+            handAnimator.SetFloat("Trigger", triggerValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Trigger", 0f);
+        }
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+            handAnimator.SetFloat("Grip", gripValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Grip", 0f);
+        }
+    }
+
+    void tryInitialize()
+    {
         List<InputDevice> devices = new List<InputDevice>();
-        
+
         InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
 
-        if(devices.Count > 0)
+        if (devices.Count > 0)
         {
             targetDevice = devices[0];
             GameObject prefab = controllers.Find(controller => controller.name == targetDevice.name);
@@ -37,26 +90,8 @@ public class HandPresence : MonoBehaviour
             }
 
             spawnedHandModel = Instantiate(handModelPrefab, transform);
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue);
-
-        if (showController)
-        {
-            spawnedHandModel.SetActive(false);
-            spawnedController.SetActive(true);
-        }
-        else
-        {
-            spawnedController.SetActive(false);
-            spawnedHandModel.SetActive(true);
+            handAnimator = spawnedHandModel.GetComponent<Animator>();
         }
 
-
-        
     }
 }
